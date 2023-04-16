75a910a7ad1af83beaea25b98d97470f30d7754a
Rebecca Smith <rebecca.smith1220.rs@gmail.com>
2023-04-15 22:31:34 -0600

    creacion de un json usando python

---
+++ creador.py
@@ -0,0 +1,41 @@
+from faker import Faker
+import json
+
+# Initialize Faker instance
+fake = Faker()
+count = 0
+lista=[]
+
+# Generate fake purchase data
+while count <= 1000:
+    purchase_data = {
+        fake.random_int(min=1000, max=9999): {
+            'customer info': {
+                'Name': fake.name(),
+                'Age': fake.random_int(min=18, max=80, step=1),
+                'Address': fake.address(),
+                'Email': fake.email(),
+                'Phone Number': fake.phone_number(),
+            },
+            'purchase info': {
+                'Date': fake.date_between(start_date='-1y', end_date='today').strftime('%Y-%m-%d'),
+                'Time': fake.time(),
+                'Total Price': fake.pyfloat(left_digits=2, right_digits=2, positive=True),
+                'Payment Type': fake.random_element(elements=('Credit Card', 'Debit Card', 'Cash')),
+            },
+            'product info': {
+                'Product ID': fake.random_int(min=1000, max=9999, step=1),
+                'Product Name': fake.word(),
+                'Quantity': fake.random_int(min=1, max=10, step=1),
+            }
+        },
+    }
+    count +=1
+    lista.append(purchase_data)
+
+# Create JSON document from purchase data with indentation
+with open("data.json", "w") as f:
+    json.dump(lista, f, indent=4)
+
+# Print a message to confirm that the file was written
+print("Data written to data.json")
